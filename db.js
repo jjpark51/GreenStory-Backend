17ab1d3e272758f6c65958a1519acad129514925
박진석 <68769481+jjpark51@users.noreply.github.com>
2023-03-13 19:21:43 +0900

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2"
 export const db = mysql.createPool({
     host:"localhost",
     user:"root",
-    password:"Baseball101!",
+    password:"password",
     database: 'greenstory'
 
 })
